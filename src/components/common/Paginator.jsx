ac52d8e8c9aa004c21b833df20161a8526a6665f
kulebiaka <mfissyuk20@gmail.com>
2023-12-02 15:50:28 +0100

    add:paginator

---
+++ src/components/common/Paginator.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+const Paginator = ({elementsCount, pageSize, currentPage, setCurrentPage, portionSize}) => {
+
+  let dispatch = useDispatch()
+
+  let pagesCount = Math.ceil(elementsCount / pageSize)
+  
+  let portionCount = Math.ceil(pagesCount / portionSize)
+  let [currentPortion, setCurrentPortion] = useState(240)
+  
+  let pages = []
+  for(let i = (currentPortion-1) * portionSize + 1 ; (i <= currentPortion * portionSize) && (i <= pagesCount); i++){
+    pages.push(i)
+  }
+
+  let pagesComponents = pages.map(p => (<button disabled={p===currentPage}onClick={(e) => {dispatch(setCurrentPage(p))}}>{p}</button>))
+
+  return(<div>
+    <button disabled={currentPortion === 1} onClick={() => {setCurrentPortion(n => n-1)}}>Prev</button>
+    <div>
+      {pagesComponents}
+    </div>
+    <button disabled={currentPortion === portionCount} onClick={() => {setCurrentPortion(n => n+1)}}>Next</button>
+  </div>)
+}
+
+export default Paginator
